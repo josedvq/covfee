c6fff4c21c8edec335d26f3a736da4f7e9f49805
Jose Vargas <josedvq@gmail.com>
2020-08-11 16:55:30 -0600

    naming change, rta_game -> covfee

---
+++ covfee/constants.py
@@ -0,0 +1,2 @@
+PROJECTS_PATH = '/Users/josedvq/furnace/covfee/covfee/projects'
+DATABASE_PATH = '/Users/josedvq/furnace/covfee/covfee/database.db'
--- rta_game/constants.py
+++
@@ -1,2 +0,0 @@
-PROJECTS_PATH = '/Users/josedvq/furnace/covfee/rta_game/projects'
-DATABASE_PATH = '/Users/josedvq/furnace/covfee/rta_game/database.db'
