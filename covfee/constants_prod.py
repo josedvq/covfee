6d0346c6ad05b2f671b9519ffb1ec12f55472d2e
Jose Vargas <josedvq@gmail.com>
2020-09-10 23:41:54 -0600

    fixed bug in prod config

--- covfee/constants_prod.py
+++ covfee/constants_prod.py
@@ -1,3 +1,4 @@
+import os
 ENVIRONMENT = 'production'
 PROJECTS_PATH = 'projects'
 DATABASE_PATH = 'database.db'
@@ -6,3 +7,4 @@ STATIC_URL = 'http://helix.ewi.tudelft.nl/covfee-static'
 APP_PORT = 80
 API_URL = APP_URL + '/api'
 BUNDLE_URL = STATIC_URL + '/bundle.js'
+DATABASE = f'sqlite:///{os.path.dirname(os.path.realpath(__file__))}/database.db'
