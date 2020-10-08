4413480089c9103fba8a4b8800addc77172fd448
Jose Vargas <josedvq@gmail.com>
2020-10-08 19:24:34 +0200

    made it easier to use custom config

--- covfee/orm/__init__.py
+++ covfee/orm/__init__.py
@@ -6,6 +6,18 @@ from .project import *
 from .task import *
 from .user import *
 
+
+def extend_config(app):
+    app.config.update(
+        APP_URL= app.config['BASE_URL'] + '/#',
+        ADMIN_URL= app.config['BASE_URL'] + '/admin#',
+        API_URL= app.config['BASE_URL'] + '/api',
+        AUTH_URL= app.config['BASE_URL'] + '/auth',
+        STATIC_URL= app.config['BASE_URL'] + '/static',
+        MEDIA_URL= app.config['BASE_URL'] + '/media'
+    )
+
+# load the config files
 app.config.from_object('covfee.config')
 app.config.from_json(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'covfee.env.json'))
 if app.config['FLASK_ENV'] == 'development':
@@ -14,5 +26,7 @@ if app.config['FLASK_ENV'] == 'development':
 else:
     app.config.from_pyfile(os.path.join(
         os.getcwd(), 'covfee.production.config.py'))
+# apply extended config
+extend_config(app)
 app.app_context().push()
 db.init_app(app)
