56eec70727e4e408f8c4bb3434b687b693808557
Jose Vargas <josedvq@gmail.com>
2021-04-23 03:16:59 +0200

    removing instructions btn (hack)

--- covfee/client/tasks/instructions.tsx
+++ covfee/client/tasks/instructions.tsx
@@ -25,7 +25,7 @@ class InstructionsTask extends CovfeeComponent<Props, any> {
         return <Row style={{margin: '2em 0'}}>
             <Col sm={{span:22, offset:1}} md={{span: 20, offset: 2}} lg={{span:16, offset: 4}} xl={{span: 14, offset: 5}}>
                     <MarkdownLoader content={this.props.spec.content}/>
-                    <Button type="primary" onClick={this.handleSubmit}>Start!</Button>
+                    {/* <Button type="primary" onClick={this.handleSubmit}>Start!</Button> */}
             </Col>
         </Row>
     }
