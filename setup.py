6bf5aa92156058b5ac9c10836b01836380c666e0
Jose Vargas <josedvq@gmail.com>
2022-11-14 16:26:54 +0100

    fixing werkzeug due to bug

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
     },
     install_requires=[
         'Flask == 1.*',
+        'werkzeug==2.0.3',
         'MarkupSafe==2.0.1',
         'flask_cors == 3.*',
         'Flask-SQLAlchemy == 2.*',
