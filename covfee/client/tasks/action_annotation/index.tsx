b343fbc6572aa19a2321a27e86167cfe585a7c6e
Kenneth-Funes-TU-Delft <157802575+Kenneth-Funes-TU-Delft@users.noreply.github.com>
2024-02-29 10:27:44 +0100

    Added dropdowns and interactive gallery to see participants (#5)
    - The proposed AnnotationTypeDropdown became unnecessary because it did
    not add any new functionality that the base type Dropdown did not have, so
    it was removed.
    - Instantiated Dropdown now for both action selection and participant
    selection, while managing their state in Redux
    - Added custom styling and trying to have a functional sidebar with the
    video playback on the right
    - Added interactive gallery as a fullscreen overlay
    - The gallery image is the image reference for annotators to identify
    participants.
    - A button was added to display the gallery
    - The gallery is displayed as a modal overlay (fullscreen, steal keyboard
    input)
    - Added a simple logic to set the participant to annotate based on a click
      on the gallery image. This is easy given the gallery image is a regular
      grid.
    - Note: the image was made svg because there seem to be rules in the
      project in which only svg images are picked up. Unclear how to change atm

--- covfee/client/tasks/action_annotation/index.tsx
+++ covfee/client/tasks/action_annotation/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { slice, actions, State } from "./slice";
 import { TaskExport } from "../../types/node";
 import { CovfeeTaskProps } from "../base";
@@ -7,13 +7,25 @@ import { AllPropsRequired } from "../../types/utils";
 import { useDispatch } from "../../journey/state";
 import { useSelector } from "react-redux";
 import VideojsPlayer from "../../players/videojs";
-import AnnotationTypeDropdown from "../../input/annotation_type_dropdown";
-import { MenuProps } from "antd";
+import type { MenuProps } from "antd";
+import { Dropdown, Menu, Space, MenuInfo, Button } from "antd";
+import { DownOutlined } from "@ant-design/icons";
 import styles from "./action_annotation.module.css";
+
+// You can download this image and place it in the art folder from:
+// @helix.ewi.tudelft.nl:/home/kfunesmora/mingle/media
+// Do not commit to the repo for confidentiality reasons.
+import ConflabGallery from "../../art/conflab-gallery.svg";
 
 interface Props extends CovfeeTaskProps<ActionAnnotationTaskSpec> {}
 
-const annotation_types: string[] = ["speaking", "laughing"];
+// Declare constants with the participants expected in the task, as well
+// as the annotation types to be made
+const ANNOTATION_TYPES: string[] = ["speaking", "laughing"];
+const PARTICIPANTS_LIST: string[] = [];
+for (let i = 1; i < 51; i++) {
+  PARTICIPANTS_LIST.push("Participant_" + i);
+}
 
 const ActionAnnotationTask: React.FC<Props> = (props) => {
   // here we set the defaults for the task props
@@ -27,6 +39,9 @@ const ActionAnnotationTask: React.FC<Props> = (props) => {
     },
   };
 
+  // this is a custom dispatch function provided by Covfee
+  const dispatch = useDispatch();
+
   // we read the state using useSelector
   const mediaPaused = useSelector<State, boolean>((state) => state.mediaPaused);
   const annotations = useSelector<State, Record<string, boolean[]>>(
@@ -35,48 +50,154 @@ const ActionAnnotationTask: React.FC<Props> = (props) => {
   const active_annotation = useSelector<State, string>(
     (state) => state.active_annotation
   );
-
-  // this is a custom dispatch function provided by Covfee
-  const dispatch = useDispatch();
+  const active_participant = useSelector<State, string>(
+    (state) => state.active_participant
+  );
+  const [showingGallery, setShowingGallery] = useState(false);
 
   const my_video = {
     type: "video",
     url: "https://mdn.github.io/learning-area/html/multimedia-and-embedding/video-and-audio-content/rabbit320.mp4",
   };
 
+  // We prepare the annotations options
+  const annotations_menu_items: MenuProps["items"] = [
+    {
+      key: "1",
+      type: "group",
+      label: "Select annotation",
+      children: ANNOTATION_TYPES.map((annotation_type) => ({
+        key: annotation_type,
+        label: annotation_type,
+        onClick: (item: MenuInfo) => {
+          dispatch(actions.setActiveAnnotation(item.key));
+        },
+      })),
+    },
+  ];
+
+  // We prepare the annotations options
+  const participants_menu_items: MenuProps["items"] = [
+    {
+      key: "1",
+      type: "group",
+      label: "Select participant",
+      children: PARTICIPANTS_LIST.map((participant_available) => ({
+        key: participant_available,
+        label: participant_available,
+        onClick: (item: MenuInfo) => {
+          dispatch(actions.setActiveParticipant(item.key));
+        },
+      })),
+    },
+  ];
+
+  // We use a Ref to know to which DOM element to redirect the keyboard focus
+  // and as to capture key press events. Also, to retrieve the geometry of the
+  // underlying gallery svg image.
+  const galleryOverlayRef = useRef(null);
+  useEffect(() => {
+    if (showingGallery && galleryOverlayRef.current) {
+      galleryOverlayRef.current.focus();
+    }
+  }, [showingGallery]);
+
+  // TODO: Confider moving the gallery overlay into a unique component.
+  const handleClickOnGalleryImage = (e: MouseEvent) => {
+    // Based on the known gallery image of participants, we calculate in which
+    // participant the click is falling in.
+    if (galleryOverlayRef.current) {
+      const galleryOverlayImageElement =
+        galleryOverlayRef.current.querySelectorAll("svg")[0];
+      if (galleryOverlayImageElement) {
+        const grid_size_x = 6;
+        const grid_size_y = 8;
+        var imageRect = galleryOverlayImageElement.getBoundingClientRect();
+        var cell_x = Math.floor(
+          (grid_size_x * (e.clientX - imageRect.left)) / imageRect.width
+        );
+        var cell_y = Math.floor(
+          (grid_size_y * (e.clientY - imageRect.top)) / imageRect.height
+        );
+        let participant_id: number = cell_y * grid_size_x + cell_x + 1;
+        if (participant_id >= 38) {
+          participant_id += 2;
+        }
+        dispatch(actions.setActiveParticipant("Participant_" + participant_id));
+      }
+      setShowingGallery(false);
+    }
+  };
+
   // and we render the component
   return (
     <form>
+      {showingGallery && (
+        <div
+          className={styles["gallery-overlay"]}
+          onKeyDown={(e) => {
+            e.preventDefault();
+            if (e.key === "Escape") {
+              setShowingGallery(false);
+            }
+          }}
+          tabIndex={-1}
+          ref={galleryOverlayRef}
+        >
+          <h1>Press ESC to close or click on the participant to annotate</h1>
+          <ConflabGallery
+            className={styles["gallery-overlay-image"]}
+            onClick={handleClickOnGalleryImage}
+          />
+        </div>
+      )}
       <div className={styles.action_annotation_task}>
         <div className={styles.sidebar}>
-          <AnnotationTypeDropdown
-            annotation_types={annotation_types}
-            itemClick={(item_key: string) =>
-              dispatch(actions.setActiveAnnotation(item_key))
-            }
-            selected_annotation={active_annotation}
-          />
-          <div>
-            <button
-              id="speaking"
-              value="speaking"
-              onClick={() => dispatch(actions.setActiveAnnotation("speaking"))}
-              disabled={active_annotation == "speaking"}
-            >
-              Speaking
-            </button>
-          </div>
-          <div>
-            <button
-              id="laughing"
-              value="laughing"
-              onClick={() => {
-                dispatch(actions.setActiveAnnotation("laughing"));
+          <h1>Select a participant:</h1>
+          <Dropdown
+            menu={{
+              items: participants_menu_items,
+              selectable: true,
+              className: styles["action-task-dropdown-menu"],
+            }}
+            className={styles["action-task-dropdown"]}
+          >
+            <a
+              onClick={(e) => {
+                e.preventDefault();
               }}
-              disabled={active_annotation == "laughing"}
+              className={styles["action-task-dropwdown-button"]}
             >
-              Laughing
-            </button>
+              <Space>
+                {active_participant}
+                <DownOutlined />
+              </Space>
+            </a>
+          </Dropdown>
+          <h1>Select an action to annotate:</h1>
+          <Dropdown menu={{ items: annotations_menu_items, selectable: true }}>
+            <a
+              onClick={(e) => {
+                e.preventDefault();
+              }}
+              className={styles["action-task-dropwdown-button"]}
+            >
+              <Space>
+                {active_annotation}
+                <DownOutlined />
+              </Space>
+            </a>
+          </Dropdown>
+          <div className={styles.sidebarBottom}>
+            <Button
+              type="primary"
+              className={styles["gallery-button"]}
+              onClick={() => {
+                setShowingGallery(true);
+              }}
+            >
+              Gallery
+            </Button>
           </div>
         </div>
         <div className={styles.main_content}>
--- covfee/client/tasks/action_annotation/slice.ts
+++ covfee/client/tasks/action_annotation/slice.ts
@@ -3,12 +3,14 @@ import { createSlice } from "@reduxjs/toolkit"
 export interface State {
   annotations: Record<string, boolean[]>;
   active_annotation?: string; 
+  active_participant?: string; 
   mediaPaused: boolean;
 }
 
 export const initialState: State = {
   annotations: null,
   active_annotation: null,
+  active_participant: null,
   mediaPaused: null,
 }
 
@@ -24,6 +26,9 @@ export const slice = createSlice({
     },
     setActiveAnnotation: (state, action) => {
       state.active_annotation = action.payload
+    },
+    setActiveParticipant: (state, action) => {
+      state.active_participant = action.payload
     }
   },
 })
